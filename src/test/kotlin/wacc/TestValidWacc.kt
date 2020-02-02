452931b9875846fca5d4942d083c0176f3ebd64b
Tudor Nazarie <tudor@nazarie.net>
2020-02-02 01:14:07 +0000

    Add JUnit test for valid programs

---
+++ src/test/kotlin/wacc/TestValidWacc.kt
@@ -0,0 +1,30 @@
+package wacc
+
+import org.junit.Test
+import picocli.CommandLine
+import wacc.cli.Compile
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.test.assertEquals
+
+class TestValidWacc {
+    private val enabledValidTests = true
+
+    private val expectedCode = 0
+    private val path = Paths.get("wacc_examples/valid")
+
+    private val programs = Files.walk(path).filter { Files.isRegularFile(it) }
+
+    @Test
+    fun testValid() {
+        if (enabledValidTests) {
+            programs.forEach { path: Path? ->
+                println("Testing program $path")
+                val exitCode = CommandLine(Compile()).execute(path.toString())
+                println("Got exit code $exitCode")
+                assertEquals(expectedCode, exitCode)
+            }
+        }
+    }
+}
