5d9a072efb3faf7358f3709c03a6312ecbd1e625
Tudor Nazarie <tudor@nazarie.net>
2020-02-13 14:05:01 +0000

    Make Instruction subclasses data classes

--- src/main/kotlin/wacc/ast/codegen/Instruction.kt
+++ src/main/kotlin/wacc/ast/codegen/Instruction.kt
@@ -4,16 +4,15 @@ typealias Reg = Int
 typealias Addr = Long
 
 sealed class Instruction {
-
-  class Add(val rd: Reg, val rn: Reg, val op2: Reg, val carry: Boolean = false) : Instruction()
-  class And(val rd: Reg, val rn: Reg, val op2: Reg) : Instruction()
-  class BitClear(val rd: Reg, val rn: Reg, val op2: Reg)
-  class Branch(val address: Addr, val link: Boolean = false)
-  class Compare(val rn: Reg, val rd: Reg, val negative: Boolean = false)
-  class Load(val rd: Reg, val address: Addr)
-  class Move(val rd: Reg, val op2: Reg, val negative: Boolean = false)
-  class Multiply(val rd: Reg, val rm: Reg, val rs: Reg, val rn: Reg? = null)
-  class Or(val rd: Reg, val rn: Reg, val op2: Reg)
-  class Xor(val rd: Reg, val rn: Reg, val op2: Reg) : Instruction()
+  data class Add(val rd: Reg, val rn: Reg, val op2: Reg, val carry: Boolean = false) : Instruction()
+  data class And(val rd: Reg, val rn: Reg, val op2: Reg) : Instruction()
+  data class BitClear(val rd: Reg, val rn: Reg, val op2: Reg) : Instruction()
+  data class Branch(val address: Addr, val link: Boolean = false) : Instruction()
+  data class Compare(val rn: Reg, val rd: Reg, val negative: Boolean = false) : Instruction()
+  data class Load(val rd: Reg, val address: Addr) : Instruction()
+  data class Move(val rd: Reg, val op2: Reg, val negative: Boolean = false) : Instruction()
+  data class Multiply(val rd: Reg, val rm: Reg, val rs: Reg, val rn: Reg? = null) : Instruction()
+  data class Or(val rd: Reg, val rn: Reg, val op2: Reg) : Instruction()
+  data class Xor(val rd: Reg, val rn: Reg, val op2: Reg) : Instruction()
 
 }
