8a39d879a4bb079d6599def20d16faae29ffd049
Tudor Nazarie <tudor@nazarie.net>
2020-02-20 14:02:22 +0000

    Add basic instruction types

--- src/main/kotlin/wacc/ast/codegen/types/Instruction.kt
+++ src/main/kotlin/wacc/ast/codegen/types/Instruction.kt
@@ -1,3 +1,10 @@
 package wacc.ast.codegen.types
 
-sealed class Instruction
+sealed class Instruction {
+//    data class Load(val condition: Condition, val access: MemoryAccess, val reg: Register, )
+    data class Move(val condition: Condition, val register: Register, val operand: Operand) : Instruction()
+    data class Push(val condition: Condition, val regs: List<Register>) : Instruction()
+    data class Pop(val condition: Condition, val regs: List<Register>) : Instruction()
+    data class Branch(val condition: Condition, val operand: Operand) : Instruction()
+    data class BranchLink(val condition: Condition, val operand: Operand) : Instruction()
+}
