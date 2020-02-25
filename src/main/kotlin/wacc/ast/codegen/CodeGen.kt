074bafcdab9fadfc2451147465a99c2c228f2d0a
Nat Karmios <nk4118@ic.ac.uk>
2020-02-25 04:43:59 +0000

    Add some more code generation

--- src/main/kotlin/wacc/ast/codegen/CodeGen.kt
+++ src/main/kotlin/wacc/ast/codegen/CodeGen.kt
@@ -1,15 +1,25 @@
 package wacc.ast.codegen
 
+import wacc.ast.Expr
 import wacc.ast.Func
 import wacc.ast.Program
 import wacc.ast.Stat
+import wacc.ast.codegen.types.*
 import wacc.ast.codegen.types.Condition.Always
 import wacc.ast.codegen.types.Function
 import wacc.ast.codegen.types.Instruction.*
-import wacc.ast.codegen.types.Label
 import wacc.ast.codegen.types.Operand.Imm
 import wacc.ast.codegen.types.Register.*
-import wacc.ast.codegen.types.Section
+
+
+private class CodeGenContext(val func: Func?, val vars: Map<String, Int>) {
+    fun resolveIdent(ident: String): Int? =
+            func?.params?.indexOfFirst { it.name == ident }?.let { if (it < 0) null else (it+1)*4 }
+                    ?: vars[ident]
+
+    fun withNewVar(name: String) = CodeGenContext(func, vars + (name to TODO()))
+}
+
 
 fun Program.getAsm(): String {
     val (data, text) = genCode()
@@ -33,11 +43,23 @@ private fun Func.codeGen(): Function {
     return Function(Label(name), emptyList(), false)
 }
 
+private fun Expr.genCode(ctx: CodeGenContext): Pair<List<Instruction>, Operand> = when (this) {
+    is Expr.Literal.IntLiteral -> emptyList<Instruction>() to Imm(value.toInt(), ImmType.INT)  // TODO: int vs long?
+    is Expr.Literal.BoolLiteral -> emptyList<Instruction>() to Imm(if (value) 1 else 0, ImmType.BOOL)
+    is Expr.Literal.CharLiteral -> emptyList<Instruction>() to Imm(value.toInt(), ImmType.CHAR)
+    is Expr.Literal.StringLiteral -> emptyList<Instruction>() to Operand.Label(TODO())
+    is Expr.Literal.PairLiteral -> TODO()
+    is Expr.Ident -> TODO()
+    is Expr.ArrayElem -> TODO()
+    is Expr.UnaryOp -> TODO()
+    is Expr.BinaryOp -> TODO()
+}
+
 private fun Stat.genMainFunc(): Function {
     // TODO remove hardcoded function
     return Function(Label("main"), listOf(
             Push(listOf(LinkRegister)),
-            Move(GeneralRegister(0), Imm(0)),
+            Move(GeneralRegister(0), Imm(0, ImmType.INT)),
             Pop(listOf(ProgramCounter)),
             Special.Ltorg
     ), true)
--- src/main/kotlin/wacc/ast/codegen/types/Operand.kt
+++ src/main/kotlin/wacc/ast/codegen/types/Operand.kt
@@ -1,10 +1,23 @@
 package wacc.ast.codegen.types
 
 sealed class Operand {
-    data class Imm(val value: Int) : Operand() {
-        override fun toString(): String = "#$value"
+
+    data class Imm(val value: Int, val type: ImmType) : Operand() {
+        private val display: String = when (type) {
+            ImmType.INT,
+            ImmType.BOOL -> value.toString()
+            ImmType.CHAR -> value.toChar().toString()
+        }
+
+        override fun toString(): String = "#$display"
     }
 
     data class Reg(val reg: Register) : Operand()
     data class Label(val label: String) : Operand()
+}
+
+enum class ImmType {
+    INT,
+    BOOL,
+    CHAR
 }
