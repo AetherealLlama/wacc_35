4106849f6d691bf572adcc4b8be721d6db9d8a1a
Tudor Nazarie <tudor@nazarie.net>
2020-02-28 17:24:17 +0000

    Make InitializedString extend InitializedDatum

--- src/main/kotlin/wacc/ast/codegen/types/InitializedDatum.kt
+++ src/main/kotlin/wacc/ast/codegen/types/InitializedDatum.kt
@@ -1,7 +1,7 @@
 package wacc.ast.codegen.types
 
 sealed class InitializedDatum {
-    data class InitializedString(val label: String, val length: Int, val ascii: String) {
+    data class InitializedString(val label: String, val length: Int, val ascii: String) : InitializedDatum() {
         override fun toString(): String {
             val builder = StringBuilder()
             builder.append("$label:\n")
